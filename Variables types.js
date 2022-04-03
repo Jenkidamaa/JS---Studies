74fb0df9a0fb97163e2b885bec0db5ca7230a53c
Carlos Isaque Da Silva <85850597+Jenkidamaa@users.noreply.github.com>
2022-04-02 22:33:33 -0300

    Update Variables types.js

--- Variables types.js
+++ Variables types.js
@@ -8,16 +8,5 @@ let boolena = true
 let sobre = undefined
 let nula = null
 
-//Declaration of objects
 
-let pessoa = {
-    nome : 'Charlles',
-    idade: 25,
-    nota1: 8,
-    nota2: 7,
-    media: (8+7)/2,
-    
-    aprovate : true,
-}
-console.log(pessoa.media);
-pessoa.nome
+
