bea807fe756fc0fc7162ad8549358a17eac04348
Carlos Isaque Da Silva <85850597+Jenkidamaa@users.noreply.github.com>
2022-04-02 22:30:44 -0300

    Update Variables types.js

--- Variables types.js
+++ Variables types.js
@@ -1,6 +1,23 @@
-let name = Challes 
+// declaration of variables
+
+
+let name = 'Challes'
 let numberInt = 25
 let numberFloat = 25.6
-let boolena = True
+let boolena = true
 let sobre = undefined
 let nula = null
+
+//Declaration of objects
+
+let pessoa = {
+    nome : 'Charlles',
+    idade: 25,
+    nota1: 8,
+    nota2: 7,
+    media: (8+7)/2,
+    
+    aprovate : true,
+}
+console.log(pessoa.media);
+pessoa.nome
