7d480eb5bb3c146834ccc5da7ecdb40ef1211f60
Carlos Isaque Da Silva <85850597+Jenkidamaa@users.noreply.github.com>
2022-06-14 22:04:52 -0300

    Create exec.js

---
+++ exec.js
@@ -0,0 +1,21 @@
+function vezes10(texto) {
+  let a = 0
+  let b = ""
+  while(a < 10){
+    b = b + texto
+    a = a +1
+  }
+  return b
+}
+
+function multiplica(texto, repeticoes) {
+  let a = ""
+  let b = repeticoes
+  let c = 0
+  while(c<b){
+      a = a + texto
+      c = c + 1
+
+  }
+  return a
+}
